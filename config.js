9bf1e9fd55bc5dbcd14c383d4cfd66de95703192
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2023-08-11 15:10:12 +0100

    config update
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   "pruneStaleBranches": true,
   "recreateWhen": "always",
   "onboarding": false,
-  "requireConfig": false,
+  "requireConfig": "optional",
   "baseBranches": ["master", "main"],
   "packageRules": [
 
